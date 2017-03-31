d6dcd477666a76e81acb8333a97fab21d80a13d7
Sathyanarayana Mahendran <sathyanarayana.mahendran@gmail.com>
2017-03-30 21:03:19 -0400

    committing 14 examples from MAC Laptop

---
+++ ex3.py
@@ -0,0 +1,32 @@
+print "I will no count my chickens:"
+
+#Divides 30 by 6, then add the result with 25
+print "Hens", 25 + 30 / 6  #30
+
+# Multplies 25 and 3 , then result is modulo by 4 which is 3, 100-3
+print "Roosters", 100 - 25 * 3 % 4 #97
+
+print "Now I will count the eggs:"
+
+#Divides 1/4 which is 0 (as it is  not floating point value , we get 0. Either use 1.0 or 4.0 , you get result as .25)
+#Modulo 4%2 =0
+# 3+2+1 =6 , 5+0 = 5, 0+6 =6
+# 6-5+6 -> 1 +6 = 7
+#Order of Operations - PEMDAS
+# Parenthisi, Exponents, Multiplication, Division, Addition, Substraction 
+print 3 + 2 + 1  - 5 + 4 % 2 - 1 / 4 + 6 #
+
+print "Is it true that 3 +2 <5 - 7?"
+
+print 3 + 2 < 5 -7 # false
+
+print "What is 3 + 2?", 3 + 2 #5
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greather?", 5 > -2 # true
+print "Is it greater than equal?", 5 >= -2 #False
+print "Is it less or equal?", 5 <= -2 # False
